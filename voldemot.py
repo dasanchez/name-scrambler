6556e214040983ead9afe6c145c307a394e606c5
Dante <dante.a.sanchez@gmail.com>
2018-10-21 09:40:25 -0400

    CLI argument parsing

--- voldemot.py
+++ voldemot.py
@@ -1,60 +1,67 @@
 """
-voldemot
+voldemot - a word de-scrambler by @dasanchez
 
 Takes a set of letters (such as a name) and assembles combinations of words based on it.
-Using English word list from SCOWL.
+Used the English word list from SCOWL.
 
 Usage:
-python voldemot letters [filename] [slots]
+usage: voldemot.py [-h] [-d DICTIONARY] [-c COUNT] [-v] [-p] input
+
+positional arguments:
+  input                 the letters to de-scramble
+
+optional arguments:
+  -h, --help
+                        show this help message and exit
+  -d DICTIONARY, --dictionary DICTIONARY
+                        dictionary file: newline must separate all words
+  -c COUNT, --count COUNT
+                        number of words to split the letters into
+  -v, --verbose
+                        show progress
+  -p, --print
+                        print results
 
 For example,
-python voldemot.py albertcamus words/voldemot-dict.txt 2
-
-will yield a text file called albert-camus.iter with 113
-combinations, from "ablate scrum" to "tsamba ulcer".
+python voldemot.py albertcamus -d words/voldemot-dict.txt -c 3 -p
+will print 113 combinations, from "ablate scrum" to "tsamba ulcer".
 """
-
-import sys
 import re
 import time
-from collections import Counter
+import argparse
 import asyncio
 import voldemot_utils as vol
 
 def main(args):
     """ main program """
-
-    if len(args) < 2:
-        print("Not enough arguments.\nUsage:\npython voldemot.py letters [filename]")
-
     # load source soup
-    letters = args[1]
+    letters = args.input
 
     # remove spaces and non-alphabetical characters
     letters = re.sub(r"\W?\d?", "", letters).lower()
-    print("Source soup (" + letters + ") has " + str(len(letters)) + " characters")
+    if args.verbose:
+        print("Source soup (" + letters + ") has " +
+              str(len(letters)) + " characters")
 
     # generate dictionary file name
-    wordFileName = "words/voldemot-dict.txt"
-    if len(args) > 3:
-        wordFileName = args[2]
+    wordFileName = args.dictionary
 
     # set number of spots
-    wordCount = 3
-    if len(args) > 3:
-        wordCount = int(args[3])
+    wordCount = args.count
 
     # make a list of candidate words
     wordsFound = vol.getWordList(wordFileName, letters)
-    print("Found " + str(len(wordsFound)) + " words.")
+    if args.verbose:
+        print("Found " + str(len(wordsFound)) + " words.")
 
     start = time.time()
 
     fullMatch = []
-    # # generate all possible and put them in the fullMatch list
+    # generate all possible and put them in the fullMatch list
     loop = asyncio.get_event_loop()
     if wordCount >= 2:
-        fullMatch = loop.run_until_complete(vol.findWordCombinations(wordsFound, letters, wordCount, 10000, 0.05))
+        fullMatch = loop.run_until_complete(vol.findWordCombinations(
+            wordsFound, letters, wordCount, 10000, 0.05, args.verbose))
     else:
         # Default to single words:
         for word in vol.getWordsEqualTo(wordsFound, len(letters)):
@@ -62,11 +69,27 @@ def main(args):
                 fullMatch.append(word)
     loop.close()
     end = time.time()
-    print(f"{(end-start):.2f} seconds elapsed.")
-    print(f"There are {len(fullMatch)} full matches.")
 
-    for entry in fullMatch:
-        print(entry)
+    print(f"{(end-start):.2f} seconds elapsed, there are {len(fullMatch)} full matches.")
+
+    if args.print:
+        for entry in fullMatch:
+            print(entry)
+
 
 if __name__ == "__main__":
-    main(sys.argv)
+    # validate input
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="the letters to de-scramble")
+    parser.add_argument("-d", "--dictionary",
+                        help="override the default dictionary file",
+                        default="words/voldemot-dict.txt")
+    parser.add_argument("-c", "--count",
+                        help="number of words to combine (default: 2)",
+                        type=int, default=2)
+    parser.add_argument("-v", "--verbose",
+                        help="show progress", action="store_true")
+    parser.add_argument(
+        "-p", "--print", help="print results", action="store_true")
+    para = parser.parse_args()
+    main(para)
